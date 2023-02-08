d6a96d61da09d53f55d0738b6cec48c905565286
ZofiaSamsel <79842403+ZofiaSamsel@users.noreply.github.com>
2023-02-08 15:54:10 +0100

    update

---
+++ main.py
@@ -0,0 +1,252 @@
+# !/usr/bin/env python
+# -*- coding: latin-1 -*-
+import codecs
+import random
+import csv
+from os.path import join
+import yaml
+from psychopy import visual, event, gui, core
+from bhaptics import better_haptic_player as player
+import atexit
+
+#--------------------------------------------------
+#read text from file or add some extra text
+#file_name: the name of the file to read
+#return: message
+#--------------------------------------------------
+def read_text_from_file(file_name, insert=''):
+    msg = list()
+    with codecs.open(file_name, encoding='utf-8', mode='r') as data_file:
+        for line in data_file:
+            if not line.startswith('#'):  # if not commented line
+                if line.startswith('<--insert-->'):
+                    if insert:
+                        msg.append(insert)
+                else:
+                    msg.append(line)
+    return ''.join(msg)
+
+#--------------------------------------------------
+#show the info; with the SPACE button
+#--------------------------------------------------
+def show_info(win, file_name, insert=''):
+    msg = read_text_from_file(file_name, insert=insert)
+    msg = visual.TextStim(win, color=conf['TEXT_COLOR'], text=msg, height=conf['TEXT_SIZE'])
+    msg.draw()
+    win.flip()
+    key = event.waitKeys(keyList=['g', 'space'])
+    if key == ['g']:
+        win.close()
+        core.quit()
+    win.flip()
+
+#--------------------------------------------------
+#show the info; without the SPACE button
+#--------------------------------------------------
+def show_info_br(win, file_name, insert=''):
+    msg = read_text_from_file(file_name, insert=insert)
+    msg = visual.TextStim(win, color=conf['TEXT_COLOR'], text=msg, height=conf['TEXT_SIZE'])
+    msg.draw()
+    win.flip()
+    key = event.getKeys(keyList=['g'])
+    if key == ['g']:
+        win.close()
+        core.quit()
+
+#--------------------------------------------------
+#save data in the .csv file
+#--------------------------------------------------
+def save_data():
+    with open(join('results', datafile), "w", newline='') as df:
+        write = csv.writer(df)
+        write.writerows(RESULTS)
+
+    #create a method which submits patterns according to index 
+def play(index):
+    if index == '1_L':
+        print("submit 1_L")
+        player.submit_registered("1_L")  
+    elif index == '2_L':
+        print("submit 2_L")
+        player.submit_registered("2_L")
+    elif index == '3_L':
+        print("submit 3_L")
+        player.submit_registered("3_L")
+    elif index == '1_R':
+        print("submit 1_R")
+        player.submit_registered("1_R")  
+    elif index == '2_R':
+        print("submit 2_R")
+        player.submit_registered("2_R")
+    elif index == '3_R':
+        print("submit 3_R")
+        player.submit_registered("3_R")
+    return index
+        # print("submit Circle With Diff AltKey")
+        # player.submit_registered_with_option("Circle", "alt2",
+        #                                      scale_option={"intensity": 1, "duration": 1},
+        #                                      rotation_option={"offsetAngleX": 0, "offsetY": 0})
+
+def run_trial(win):
+    global key, rt, corr, pattern, stim_type, prev_stim
+
+    # losowanie bodzca tak, ze nie ma dwoch takich samych po sobie
+    stim_type = random.choice(list(stim))
+    while stim_type == prev_stim:
+        stim_type = random.choice(list(stim))
+    prev_stim = stim_type
+
+    # punkt fiksacji
+    fix.setAutoDraw(True)
+    win.flip()
+    core.wait(conf['FIX_CROSS_TIME'])  # wyswietlanie samego punktu fiksacji
+
+    # rozpoczecie trialu
+    event.clearEvents()
+    win.callOnFlip(clock.reset)
+
+    play(stim_type)
+    pattern = stim_type
+    win.flip()
+
+    # czekanie na reakcje
+    while clock.getTime() <= conf['TIME_MAX']:
+        key = event.getKeys(conf['REACTION_KEYS'])
+        if key == ['q'] or key == ['p']:
+            rt = clock.getTime()
+            break
+        if key == ['g']:
+            win.close()
+            core.quit()
+   
+    if clock.getTime() > conf['TIME_MAX']:
+        rt = '-'
+
+
+    # stim[stim_type].setAutoDraw(False)
+    fix.setAutoDraw(False)
+    win.flip()
+
+    # przerwa pomiedzy trialami
+    core.wait(conf['STIM_BREAK'])
+
+    # pattern 
+    if (stim_type == "1_L") or (stim_type == "1_R"):
+        pattern = 1
+    elif (stim_type == "2_L") or (stim_type == "2_R"):
+        pattern = 2
+    elif (stim_type == "3_L") or (stim_type == "3_R"):
+        pattern = 3
+
+    # corr = poprawnosc
+    if (stim_type == "1_L" and key == ['q']) or (stim_type == "2_L" and key == ['q']) or (stim_type == "3_L" and key == ['q']) or \
+        (stim_type == "1_R" and key == ['p']) or (stim_type == "2_R" and key == ['p']) or (stim_type == "3_R" and key == ['q']):
+        corr = 1
+    elif (stim_type == "1_L" and key == ['p']) or (stim_type == "2_L" and key == ['p']) or (stim_type == "3_L" and key == ['p']) or \
+        (stim_type == "1_R" and key == ['q']) or (stim_type == "2_R" and key == ['q']) or (stim_type == "3_R" and key == ['q']):
+        corr = 0
+    else:
+        corr = "-"
+
+    RESULTS.append([ID, trial_no, train, pattern, corr, rt])
+
+
+
+#-----------------------------------------------------------------------------
+# main
+clock = core.Clock()
+#create player
+player.initialize()
+
+#register patterns
+print("register 1_L")
+player.register("1_L", "1_L.tact")
+print("register 2_L")
+player.register("2_L", "2_L.tact")
+print("register 3_L")
+player.register("3_L", "3_L.tact")
+print("register 1_R")
+player.register("1_R", "1_R.tact")
+print("register 2_R")
+player.register("2_R", "2_R.tact")
+print("register 3_R")
+player.register("3_R", "3_R.tact")
+
+# zaladowanie pliku config z parametrami
+conf = yaml.load(open('config.yaml', encoding='utf-8'), Loader=yaml.FullLoader)
+
+# ustawienia wizualne dla okna dialogowego
+# window = visual.Window(units="pix", color=conf['BACKGROUND_COLOR'], fullscr=False, size=(400, 400))
+# window.setMouseVisible(True)
+
+# okno dialogowe
+info = {'ID': '', 'PLEC': ['M', 'K'], 'WIEK': ''}
+dlg = gui.DlgFromDict(info, title='Wpisz swoje dane :)')
+if not dlg.OK:
+    print("User exited")
+    core.quit()
+
+# Og�lne ID badanych zlozone z informacji podanych w oknie dialogowym
+ID = info['ID'] + info['PLEC'] + info['WIEK']
+
+# nazwa pliku csv z wynikami badanego
+datafile = '{}.csv'.format(ID)
+
+# ustawienia okna na czas eksperymentu
+window = visual.Window(units="pix", color=conf['BACKGROUND_COLOR'], fullscr=False, size=(500, 500))
+window.setMouseVisible(True)
+
+# bodzce
+fix = visual.TextStim(win=window, text="+", color=conf['FIX_CROSS_COLOR'], height=conf['FIX_CROSS_SIZE'])
+
+stim = ('1_L','1_R', '2_L','2_R', '3_L','3_R')
+
+# informacje o eksperymencie, instrukcje
+show_info(window, join('.', 'messages', 'instr.txt'))
+show_info(window, join('.', 'messages', 'instr2.txt'))
+
+# trening
+show_info(window, join('.', 'messages', 'train_mess.txt'))
+
+RESULTS = [["PART_ID", "TRIAL", "TRAINING","PATTERN", "CORRECT", "LATENCY"]]
+
+for block_no in range(conf['NO_BLOCK_TRAIN']):
+    for a in range(conf['N_TRIALS_TRAIN']):
+        if a == 0:
+            prev_stim = '0'
+        trial_no = a + 1
+        train = 1
+        run_trial(window)
+    window.flip()
+
+# eksperyment
+show_info(window, join('.', 'messages', 'exp_mess.txt'))
+
+for block_no in range(conf['NO_BLOCK_EXP']):
+    for i in range(conf['N_TRIALS_EXP']):
+        if i == 0:
+            prev_stim = '0'
+        trial_no = i + 1
+        train = 0
+        run_trial(window)
+
+    if block_no != conf['NO_BLOCK_EXP'] - 1:
+
+        # po 0 sek od wyswietlenia bodzca nie ma reakcji na klikniecie klawisze
+        event.waitKeys(maxWait=0)
+
+        # przez TIME_FOR_REAST pakazuje sie info bez spacji
+        timer = core.CountdownTimer(conf['TIME_FOR_REAST'])
+        while timer.getTime() > 0:
+            show_info_br(window, join('.', 'messages', 'break_mess.txt'))
+        show_info(window, join('.', 'messages', 'break_mess2.txt'))
+        window.flip()
+
+# zakonczenie
+save_data()
+show_info(window, join('.', 'messages', 'fin_mess.txt'))
+window.close()
+core.quit()
+
+# if __name__ == "__main__":
+#     run()
--- sample-with-better.py
+++ sample-with-better.py
@@ -1,25 +1,17 @@
 from time import sleep
 from bhaptics import better_haptic_player as player
-import keyboard
-import pygame
+# import keyboard
+# import pygame
+from psychopy import visual, core
+import random
 
-#window's features
-background_colour = (255,0,255)
-(width, height) = (820,600)
-
-#create window
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Test PyGame')
-screen.fill(background_colour)
-
-pygame.display.flip()
 
 #create player
 player.initialize()
 
 #register patterns
 print("register CenterX")
-player.register("CenterX", "CenterX.tact")
+player.register("1_L", "1_L.tact")
 print("register Circle")
 player.register("Circle", "Circle.tact")
 print("register test")
@@ -27,90 +19,50 @@ player.register("test", "Slash9_Back_Vest.tact")
 print("register lewa_ciągły góra-dół")
 player.register("lewa_ciągły góra-dół", "lewa_ciągły góra-dół.tact")
 
-#create a method which register patterns according to index 
+#create a method which submits patterns according to index 
 def play(index):
     if index == 1:
-        print("submit LEWA/CenterX")
-        player.submit_registered("test")
+        print("submit 1_L")
+        player.submit_registered("1_L")
 
     elif index == 2:
         print("submit Circle")
-        player.submit_registered_with_option("Circle", "alt",
-                                             scale_option={"intensity": 1, "duration": 1},
+        player.submit_registered_with_option("Circle", "alt2",
+                                             scale_option={"intensity": 10, "duration": 2},
                                              rotation_option={"offsetAngleX": 180, "offsetY": 0})
     elif index == 3:
-        print("submit Circle With Diff AltKey")
-        player.submit_registered_with_option("Circle", "alt2",
-                                             scale_option={"intensity": 1, "duration": 1},
-                                             rotation_option={"offsetAngleX": 0, "offsetY": 0})
+        # print("submit Circle With Diff AltKey")
+        # player.submit_registered_with_option("Circle", "alt2",
+        #                                      scale_option={"intensity": 1, "duration": 1},
+        #                                      rotation_option={"offsetAngleX": 0, "offsetY": 0})
+        player.submit_dot("backFrame", "VestBack", [{"index": 7, "intensity": 100}], 100)
     elif index == 4:
         print("lewa_ciągły góra-dół")
         player.submit_registered("lewa_ciągły góra-dół")
     return index
 
+def trial():
+    global pattern_type, correct, latency
+    prev_stim = ''
+
+    pattern_type = random.choice((1,2,3,4))
+    while pattern_type == prev_stim:
+        pattern_type = random.choice(((1,2,3,4)))
+    prev_stim = pattern_type
+
+    play(pattern_type)
+    sleep(2)
 
 #main 
-def run(): 
-    print("Press Q to quit")
-    running = True
-    while running:
-        
-        #creating a loop to check events that are occurring
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+def run():     
 
-            #checking if keydown event happened or not
-            if event.type == pygame.KEYDOWN:
-            
-                #checking if key "A" was pressed
-                if event.key == pygame.K_q:
-                    print("Key Q has been pressed")
-                    screen.close()
-                    break
-                
-                #checking if key "J" was pressed
-                if event.key == pygame.K_j:
-                    print("Key J has been pressed")
-                    play(1)
-                    
-                #checking if key "P" was pressed
-                if event.key == pygame.K_p:
-                    print("Key P has been pressed")
-                    play(2)
-                
-                #checking if key "M" was pressed
-                if event.key == pygame.K_m:
-                    print("Key M has been pressed")
-                    play(3)
-
-    
+    # print("Press Q to quit")
 
 
+    for i in range(0,5):
+        trial()
 
-   #wait for pressed key to play pattern
-    #while True:
-     #   key = keyboard.read_key()
-      #  if key == "q" or key == "Q":
-       #     break
-        #play(int(key))
-        #elif key == "1":
-         #   play(1)
-    #    elif key == "2":
-     #       play(2)
-      #  elif key == "3":
-       #     play(3)
-        #elif key == "4":
-         #   play(4)
-        #elif key == "5":
-    #print information abot patterns, device
-    #print('=================================================')
-    #print('is_playing', player.is_playing_key())
-    #print(key)
-    #print('is_playing_key(CenterX)', player.is_playing_key('lewe_przejscie_poziome'))
-    #print('is_device_connected(Vest)', player.is_device_connected('Vest'))
-    #print('is_device_connected(ForearmL)', player.is_device_connected('ForearmL'))
-    #print('=================================================')
+
 
 
 if __name__ == "__main__":
